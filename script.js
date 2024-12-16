5c8be98d4103fc7a7de2b50d635404ecfb8e9c43
Levani migriauli <levani.migriauli@lb.ge>
2024-12-16 13:39:16 +0400

    feat & refactor: add re-usable formatCur using NumberFormat

--- script.js
+++ script.js
@@ -93,6 +93,13 @@ const formatMovementDate = function (date, locale) {
 
 }
 
+const formatCur = function (value, locale, currency) {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency
+  }).format(value)
+}
+
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = ''
 
@@ -110,11 +117,13 @@ const displayMovements = function (acc, sort = false) {
     const date = new Date(movementDate)
     const displayDate = formatMovementDate(date, acc.locale)
 
+    const formattedMovement = formatCur(movement, acc.locale, acc.currency)
+
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
         <div class="movements__date">${displayDate}</div>
-        <div class="movements__value">${movement.toFixed(2)}€</div>
+        <div class="movements__value">${formattedMovement}</div>
       </div>
     `
 
@@ -124,17 +133,17 @@ const displayMovements = function (acc, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0)
-  labelBalance.textContent = `${acc.balance.toFixed(2)}€`
+  labelBalance.textContent = formatCur(acc.balance, acc.locale, acc.currency)
 }
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements.filter(mov => mov > 0).
     reduce((acc, mov) => acc + mov, 0)
-  labelSumIn.textContent = `${incomes.toFixed(2)}€`
+  labelSumIn.textContent = formatCur(incomes, acc.locale, acc.currency)
 
   const out = acc.movements.filter(mov => mov < 0).
     reduce((acc, mov) => acc + mov, 0)
-  labelSumOut.textContent = `${Math.abs(out.toFixed(2))}€`
+  labelSumOut.textContent = formatCur(Math.abs(out), acc.locale, acc.currency)
 
   const interest = acc.movements.filter(mov => mov > 0).
     map(deposit => (deposit * acc.interestRate) / 100).
@@ -142,7 +151,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1
     }).
     reduce((acc, int) => acc + int, 0)
-  labelSumInterest.textContent = `${interest.toFixed(2)}€`
+  labelSumInterest.textContent = formatCur(interest, acc.locale, acc.currency)
 }
 
 const createUsernames = function (accs) {
@@ -171,7 +180,6 @@ let currentAccount
 currentAccount = account1
 updateUI(currentAccount)
 containerApp.style.opacity = 100
-
 
 btnLogin.addEventListener('click', function (e) {
   // prevent form from submitting
